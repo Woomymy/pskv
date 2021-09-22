f8c2da0d0f659c71fa08be693128a3dd48e9e78a
Woomymy <github@woomy.ovh>
2021-09-22 18:19:33 +0200

    SkvClient: Implement has

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,3 @@ try {
 } catch (e) {
   console.log(e);
 }
-
-interface TestObject {
-  name: string;
-  t: number;
-  tk: {
-    te: string;
-  };
-}
-await client.set("name", "value");
-await client.set("name2", { name: "hello", t: 5, tk: { te: "hello" } });
-await client.set("name3", Buffer.from("io"));
-
-await client.set("arr", ["hello", 5]);
-
-console.log(typeof (await client.get<TestObject>("name2")).t);
-console.log((await client.get("name")) == null);
-await client.delete("name2");
-console.log((await client.get<Array<unknown>>("arr")).length);
-setTimeout(async () => {
-  await client.clear();
-}, 10 * 1000);
