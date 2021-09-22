793e44e2e836b929c531b2141cc3bdbb89b63f49
Woomy <woomy@woomy.be>
2021-09-22 17:32:23 +0200

    SkvClient: Implement get

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,23 @@ try {
   console.log(e);
 }
 
+interface TestObject {
+  name: string;
+  t: number;
+  tk: {
+    te: string;
+  };
+}
 await client.set("name", "value");
 await client.set("name2", { name: "hello", t: 5, tk: { te: "hello" } });
 await client.set("name3", Buffer.from("io"));
 
-await client.delete("name");
+await client.set("arr", ["hello", 5]);
 
+console.log(typeof (await client.get<TestObject>("name2")).t);
+console.log((await client.get("name")) == null);
+await client.delete("name2");
+console.log((await client.get<Array<unknown>>("arr")).length);
 setTimeout(async () => {
   await client.clear();
 }, 10 * 1000);
