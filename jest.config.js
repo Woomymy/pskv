6726030a5df60a0c1777a1c1a92d00cc59efd54e
Woomymy <github@woomy.ovh>
2021-09-24 14:18:02 +0200

    Project: Fix imports with Jest + TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
   preset: 'ts-jest',
+  resolver: "jest-ts-webcompat-resolver",
   testEnvironment: 'node',
   globals: {
     "ts-jest": {
