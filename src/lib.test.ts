4c7041e82ed560d9492522a41480cf27f56ed606
Woomy <woomy@woomy.be>
2021-09-24 14:18:29 +0200

    Tests: Add .set tests

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -5,4 +5,18 @@ describe("Database tests", () => {
         test.only("Skipping tests", async () => {
             console.warn("Skipping other tests");
         });
+    const client = new Skv({
+        dbConfig: {
+            host: process.env.DB_HOST,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            user: process.env.POSTGRES_USER
+        }
+    });
+    test(".set works", async () => {
+        await client.connect();
+        client.set("name", "Emily");
+        expect(await client.get("name")).toBe("Emily");
+        await client.end();
+    });
 });
