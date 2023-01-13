2d36e8a5bff0dad1b1688467a664ff95bda588d2
Woomymy <woomy@woomy.be>
2023-01-13 21:25:27 +0100

    jest/config: Use 'transform' for ts-jest
    Using "globals" is deprecated
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: 'ts-jest',
-  resolver: "jest-ts-webcompat-resolver",
-  testEnvironment: 'node',
-  globals: {
-    "ts-jest": {
-      "tsconfig": "tsconfig.json",
-      "useESM": true,
-    }
-  },
-  testPathIgnorePatterns: ["dist/"],
-  transform: {},
-  extensionsToTreatAsEsm: [".ts"]
+    preset: "ts-jest",
+    resolver: "jest-ts-webcompat-resolver",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.[tj]sx?$": [
+            "ts-jest",
+            {
+                tsconfig: "tsconfig.json",
+                useESM: true
+            }
+        ]
+    },
+    testPathIgnorePatterns: ["dist/"],
+    extensionsToTreatAsEsm: [".ts"]
 };
